b19497ede28a2913a009a6f4a0b3f081702fcdf6
20230516_macos_git_user <add-122@hotmail.com>
2023-06-15 10:38:35 +0900

    设定网络访问，vue.config.js加入 host: '0.0.0.0'

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    host: '0.0.0.0',
     port: 8081 // 指定开发服务器的端口
   }
 })
